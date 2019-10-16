5783b75d1e07de3252802d33e0b84cbdf1023f5a
Evan Rosen <Evan@admins-MacBook-Pro.local>
2019-10-15 20:03:04 -0700

    added users seed data

--- appointments/controllers/users.js
+++ appointments/controllers/users.js
@@ -1,75 +1,37 @@
 const knex = require("../db/knex");
 
-exports.getAllBooks = function(req, res) {
-  // With Knex Raw
-  // knex.raw(`SELECT * from books`).then(books => res.json(books.rows))
-  // With Knex query builder - long hand
-  // knex
-  //   .select()
-  //   .table('books')
-  //   .then(books => res.json(books))
-
-  // With Knex query builder - short hand
-  knex("books").then(books => res.json(books));
+exports.getAllUsers = function(req, res) {
+  knex("users").then(users => res.json(users));
 };
 
-exports.getOneBook = function(req, res) {
-  // With Knex Raw
-  // knex.raw(`SELECT * from books WHERE id = ${req.params.id}`).then(book => res.json(book.rows))
-
-  // With Knex query builder
-  // knex
-  //   .select()
-  //   .table('books')
-  //   .where('id', req.params.id)
-  //   .then(book => res.json(book))
-
-  // With Knex query builder - short hand
-  knex("books")
+exports.getOneUser = function(req, res) {
+  knex("users")
     .where("id", req.params.id)
-    .then(books => res.json(books));
+    .then(users => res.json(users));
 };
 
-exports.addOneBook = function(req, res) {
-  // With Knex Raw
-  // knex.raw(
-  //  `INSERT INTO books (title, author, pages, genre_id) VALUES ('${req.body.title}', '${req.body.author}', ${req.body.pages}, ${genre_id})`
-  //  )
-
-  // With Knex query builder
-  knex("books")
+exports.addOneUser = function(req, res) {
+  knex("users")
     .insert(req.body)
     .returning("*")
-    .then(newBook => res.json(newBook));
+    .then(newUser => res.json(newUser));
 };
 
-exports.updateOneBook = function(req, res) {
-  // With Knex Raw
-  // knex.raw(
-  //  `UPDATE books SET title = '${req.body.title}', author = '${req.body.author}', pages = ${req.body.pages}, genre_id = ${genre_id}) WHERE id = ${req.params.id}`
-  //  )
-
-  // With Knex query builder
-  knex("books")
+exports.updateOneUser = function(req, res) {
+  knex("users")
     .update({
       ...req.body,
       updated_at: new Date()
     })
     .where("id", req.params.id)
     .returning("*")
-    .then(updatedBook => res.json(updatedBook));
+    .then(updatedUser => res.json(updatedUser));
 };
 
-exports.removeOneBook = function(req, res) {
-  // With Knex Raw
-  // knex.raw(
-  //  `DELETE FROM books WHERE id = ${req.params.id}`
-  // )
-
-  // With Knex query builder
-  knex("books")
+exports.removeOneUser = function(req, res) {
+  knex("users")
     .del()
     .where("id", req.params.id)
     .returning("*")
-    .then(newBook => res.json(newBook));
+    .then(newUser => res.json(newUser));
 };
--- appointments/db/migrations/01_users.js
+++ appointments/db/migrations/01_users.js
@@ -2,7 +2,10 @@ exports.up = function(knex) {
   return knex.schema.createTable("users", function(table) {
     table.increments();
     table.string("name").notNullable();
+    table.string("company").notNullable();
     table.string("email").notNullable();
+    table.string("phone").notNullable();
+    table.string("address").notNullable();
     table.string("password").notNullable();
     table.timestamps(true, true);
   });
